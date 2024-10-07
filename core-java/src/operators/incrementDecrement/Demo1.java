adf18f2b433953ce6a8f05a0a94e6b0006c79a61
JavaBySandeep <42215856+javabysandeep@users.noreply.github.com>
2024-10-07 11:46:31 +0530

    operators 2

---
+++ core-java/src/operators/incrementDecrement/Demo1.java
@@ -0,0 +1,18 @@
+package operators.incrementDecrement;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        //System.out.println(20++);//++, -- will work on variables
+        //System.out.println(true++);// ++, -- won't work on boolean
+        int a = 10;
+        int b = ++a; // a = 11, b=11
+        System.out.println(a);//11
+        System.out.println(b);//11
+
+        int c = 10;
+        int d = --c; // c= 9, d= 9
+        System.out.println(c);//9
+        System.out.println(d);//9
+
+    }
+}
---
+++ core-java/src/operators/incrementDecrement/Demo3.java
@@ -0,0 +1,16 @@
+package operators.incrementDecrement;
+
+public class Demo3 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+        System.out.println(a++ + ++b + ++a + b++ + --c + d++);//16
+        System.out.println("a \t" + a);//3
+        System.out.println("b \t" + b);//4
+        System.out.println("c \t" + c);//2
+        System.out.println("d \t" + d);//5
+
+    }
+}
