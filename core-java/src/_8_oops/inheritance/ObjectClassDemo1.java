d1dff4b058b52e96163ea8bda39e4168367cb378
JavaBySandeep <42215856+javabysandeep@users.noreply.github.com>
2024-11-29 11:17:58 +0530

    inheritance interview questions

---
+++ core-java/src/_8_oops/inheritance/ObjectClassDemo1.java
@@ -0,0 +1,10 @@
+package _8_oops.inheritance;
+
+public class ObjectClassDemo1 {
+    public static void main(String[] args) {
+
+        class A {}
+        A a = new A();
+        System.out.println(a.hashCode());
+    }
+}
